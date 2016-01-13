10083f304fdb00dfe2d2b7357cfb123a9402ac9b
Piyush Kumar <vectarpiyush31@gmail.com>
2016-01-13 15:37:18 +0530

    Made basic structure

---
+++ app/src/test/java/com/example/piyush/cop290_assignment_0/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.piyush.cop290_assignment_0;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
